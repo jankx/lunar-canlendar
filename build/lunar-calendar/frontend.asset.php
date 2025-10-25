07c9dea6fea1d909a618135c81411252316cbe0f
Puleeno Nguyen <puleeno@gmail.com>
2025-10-25 15:36:10 +0700

    update new css

--- build/lunar-calendar/frontend.asset.php
+++ build/lunar-calendar/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'f4da392b971719abfd9b');
+<?php return array('dependencies' => array(), 'version' => 'f3da392b971719abfd9b');
--- build/lunar-calendar/index.asset.php
+++ build/lunar-calendar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '174a54d9147e97ada551');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '173a54d9147e97ada551');
